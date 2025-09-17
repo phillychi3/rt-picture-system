6c7f7a0ee78f8490897843a5a309304e1721bef3
phillychi3 <phillychi3@gmail.com>
2025-09-17 10:30:07 +0800

    fix: zeabur build fail

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import autoAdapter from '@sveltejs/adapter-auto';
+import zeaburAdapter from '@zeabur/svelte-adapter';
 
 /** @type {import('@sveltejs/kit').Config} */
+const adapter = process.env.ZEABUR_SERVICE_ID ? zeaburAdapter : autoAdapter;
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
